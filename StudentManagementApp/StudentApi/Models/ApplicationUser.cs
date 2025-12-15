5158621ebca81216b0937162f8453624fd3bebbc
Pratyush <157958382+prates09@users.noreply.github.com>
2025-12-15 13:44:52 +0530

    Delete StudentManagementApp/StudentApi directory

--- StudentManagementApp/StudentApi/Models/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace StudentApi.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-    }
-}
--- StudentManagementApp/StudentApi/Models/RegisterDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StudentApi.Models
-{
-    public class RegisterDto
-    {
-        public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
-}
